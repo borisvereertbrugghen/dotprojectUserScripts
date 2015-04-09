18d0046446be2ffdeb63cfaf572756d792ca578e
Boris Vereertbrugghen <boris.vereertbrugghen@pearlchain.net>
2015-04-09 09:51:58 +0200

    april updates

--- insertData.user.js
+++ insertData.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dotproject insert data
 // @namespace    http://boris.pearlchain.net/
-// @version      0.2
+// @version      15.4
 // @description  enter something useful
 // @author       Boris
 // @match        http://dotproject.pearlchain.net/index.php?m=timecard*
@@ -25,65 +25,146 @@ function setLastTasks(taksString){
     $.cookie("dotprojectTamperLastTasks",taksString);
 }
 
-//when we are on tab 0
-var sunday = $( "table>tbody>tr>td>div[align='left']>b" ).filter(":first");//.css( "border", "3px double red" );
-var theTable = sunday.parent().parent().parent().parent().parent();
-var dayEle=null;
-var dayTotalHours=0;
-var tasksInday=new Array();
-var rows = theTable.find("tr");
-rows.each(function (index){
-    var day = $(this).find("td>div>b");
-    if(day.length){
-        var myDay=day.html();
-        console.log('found '+myDay);
-        if(dayEle!==null && dayTotalHours<8){
-            dayEle.css( "border", "1px solid red" );
-            var tasks = getLastTasks();
-        	console.log(tasks);
-            var ts = tasks.split(";");
-        	console.log(ts);
-            var links='';
-            var link = dayEle.parent().parent().find("a").attr("href");
-            for(var i = 0; i < ts.length; i++){
-                var task=ts[i];
-                console.log(task);
-                var v = task.split('-',4);
-                console.log(tasksInday);
-                if(tasksInday.indexOf(v[2])<0){
-                    links += "<span style='padding-left:20px;'> <a href='"+link+ "&entity="+v[0]+"&pro="+v[1]+"&task="+v[2]+"&hours="+(8-dayTotalHours)+"' >"+v[3]+"</a> </span>";
-                }
-            }
-            console.log(links);
-            dayEle.html(dayEle.html()+"<span style='position: absolute;right:15%;font-size:smaller'>"+links+"</span>");
+function showTasks(){
+    if(getUrlParameter('removetask')=='all'){
+        setLastTasks('');
+    }
+    var tasks=getLastTasks();
+    if(tasks===''){
+        return;
+    }
+    var ts = tasks.split(";");
+    console.log(ts);
+    var addTasks = '<td width="600">';
+    var link=$(location).attr('href');
+    var newTasks='';
+    for(var i = 0; i < ts.length; i++){
+        var task=ts[i];
+        if(task===''){
+            continue;
         }
-        if(myDay!='Sunday'&&myDay!='Saturday'){
-            dayEle=day.parent();
-            dayTotalHours=0;
-            tasksInday=new Array();
+        console.log(task);
+        var v = task.split('-',5);
+        console.log(v);
+        var name=v[3];
+        var full=v[3];
+        if(v.length>=4 && v[4]!=='' && v[4]!== undefined){
+            full=v[4];
+        }
+        if(getUrlParameter('task')==''+i){
+            console.log('doing action on task '+i);
+            if(getUrlParameter('removetask')=='yes'){
+               console.log('Removing task no '+i);
+               continue;
+            }
+        }
+        console.log(name);
+        console.log(full);
+        addTasks += "<div> "+full+" <a href='"+link+"&task="+i+"&removetask=yes'> remove </a></div>";
+        if(newTasks===''){
+            newTasks=task;
         }else{
-            dayEle=null;
-        }
-    }else{
-        if(dayEle!==null){
-            var tds = $(this).find("td");
-            if(tds.length==3){
-                var hourEle  = $(this).find("td").last();
-                var taskIdUrl = $(this).find("a").first().attr("href")
-                console.log(taskIdUrl);
-                var taskId  = taskIdUrl.substring(24);
-                console.log(taskId);
-                tasksInday.push(taskId);
-                console.log(tasksInday);
-                //hourEle.css( "border", "3px double red" );
-                var hour = parseInt(hourEle.html());
-                dayTotalHours+=hour;
-                console.log('log line '+hour+" / "+dayTotalHours);
-            }
+            newTasks=newTasks+";"+task;
         }
     }
+    console.log(newTasks);
+    setLastTasks(newTasks);
+    addTasks += "<div>  <a href='"+link+"&removetask=all'> removeAll </a></div>";
+    addTasks+='</div>';
+    var td = $("table>tbody>tr>td>table>tbody>tr>td>h1").parent();
+    td.width(500);
+    var row = td.parent();
+    row.html(row.html()+addTasks);
+}
+
+showTasks();
+//when we are on tab 0
+var sunday = $( "table>tbody>tr>td>div[align='left']>b" ).filter(":first");//.css( "border", "3px double red" );
+if(sunday.length){
+    var theTable = sunday.parent().parent().parent().parent().parent();
+    var dayEle=null;
+    var dayTotalHours=0;
+    var tasksInday=new Array();
+    var rows = theTable.find("tr");
+    rows.each(function (index){
+        var day = $(this).find("td>div>b");
+        if(day.length){
+            var myDay=day.html();
+            console.log('found '+myDay);
+            if(dayEle!==null && dayTotalHours<8){
+                dayEle.css( "border", "1px solid red" );
+                var tasks = getLastTasks();
+                console.log(tasks);
+                var ts = tasks.split(";");
+                console.log(ts);
+                var links='';
+                var link = dayEle.parent().parent().find("a").attr("href");
+                for(var i = 0; i < ts.length; i++){
+                    var task=ts[i];
+                    if(task===''){
+                        continue;
+                    }
+                    console.log(task);
+                    var v = task.split('-',4);
+                    console.log(tasksInday);
+                    if(tasksInday.indexOf(v[2])<0){
+                        links += "<span style='padding-left:20px;'>";
+                        links += "<a href='"+link+ "&entity="+v[0]+"&pro="+v[1]+"&task="+v[2]+"&hours="+(8-dayTotalHours)+"' >"+v[3]+"</a> ";
+                        links += "<a href='"+link+ "&entity="+v[0]+"&pro="+v[1]+"&task="+v[2]+"&hours="+(8-dayTotalHours)+"&autopost=1' >"+(8-dayTotalHours)+"</a> ";
+                        links += "</span>";
+                    }
+                }
+                console.log(links);
+                dayEle.html(dayEle.html()+"<span style='position: absolute;right:15%;font-size:smaller'>"+links+"</span>");
+            }
+            if(myDay!='Sunday'&&myDay!='Saturday'){
+                dayEle=day.parent();
+                dayTotalHours=0;
+                tasksInday=new Array();
+            }else{
+                dayEle=null;
+            }
+        }else{
+            if(dayEle!==null){
+                var tds = $(this).find("td");
+                if(tds.length==3){
+                    var hourEle  = $(this).find("td").last();
+                    var taskIdUrl = $(this).find("a").first().attr("href");
+                    console.log(taskIdUrl);
+                    var taskId  = taskIdUrl.substring(24);
+                    console.log(taskId);
+                    tasksInday.push(taskId);
+                    console.log(tasksInday);
+                    //hourEle.css( "border", "3px double red" );
+                    var hour = parseInt(hourEle.html());
+                    dayTotalHours+=hour;
+                    console.log('log line '+hour+" / "+dayTotalHours);
+                }
+            }
+        }
+    });
+    //add extra edit links
+    var edits = $( "a:contains('[Edit]')");
+    //edits.css( "border", "3px double red" );
+    edits.each(function (index){
+        var data = $(this).parent();
+        var hours = parseInt(data.parent().find("td").last().html());
+        var theUrl = $(this).attr("href");
+        var newData = data.html();
+        for (var i=1;i<=8;i++){
+            if(i==hours){
+                newData+="<b> "+i+" </b>";
+            }else{
+                newData+="<a href='"+theUrl+"&hours="+i+"&autopost=1' > "+i+" </a>";
+            }
+        }
+        newData+="<a href='"+theUrl+"&del="+i+"&autopost=1' >"+'<img align="absmiddle" src="./images/icons/stock_delete-16.png" width="16" height="16" alt="Delete this project" border="0"> </a>';
+        //newData+='<a href="javascript:delIt()"><img align="absmiddle" src="./images/icons/stock_delete-16.png" width="16" height="16" alt="Delete this project" border="0"></a>';
+        data.html(newData);
+    });
     
-});
+    
+}
 //when posting a task (tab 2)
 var theform = $("form[name='AddEdit']");
 if(theform.length){
@@ -152,6 +233,15 @@ if(theform.length){
     var hours = getUrlParameter('hours');
     if(hours !== undefined){
         $("input[name='task_log_hours']").val(hours);
+    }
+     var del = getUrlParameter('del');
+    if(del !== undefined){
+        $("input[name='del']").val(1);
+    }
+    
+    var autoPost = getUrlParameter('autopost');
+    if(autoPost==1){
+        $("input[name='btnFuseAction']").click();
     }
 }else{
     console.log("no Form");
